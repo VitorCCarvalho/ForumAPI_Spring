1dd72a695fc8d53fe824b9a9ef2b7d9c805c187d
Vitor Costa Carvalho <49074487+VitorCCarvalho@users.noreply.github.com>
2025-01-20 18:15:29 -0300

    Initial commit

---
+++ src/main/java/codespace/forumapi/ForumapiApplication.java
@@ -0,0 +1,13 @@
+package codespace.forumapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/codespace/forumapi/ForumapiApplicationTests.java
@@ -0,0 +1,13 @@
+package codespace.forumapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
